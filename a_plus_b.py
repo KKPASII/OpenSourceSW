2b32d1ef7df65865f3f69f7f54f1c2f085b450e8
KKPASII <kkongpasi1005@gmail.com>
2024-10-09 03:44:36 +0900

    변수 a, b 추가

--- a_plus_b.py
+++ a_plus_b.py
@@ -1,4 +1,6 @@
 x = int(input("Enter a number: "))
 y = int(input("Enter another number: "))
-print(f"{x} + {y} = {x+y}")
-print(f"{x} - {y} = {x-y}")
+a = x + y
+b = x - y
+print(f"{a} + {b} = {a+b}")
+print(f"{a} - {b} = {a-b}")
